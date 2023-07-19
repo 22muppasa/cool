702ace9bed7842e8e658eb69ab2ec2a4a862ae51
22muppasa <22muppasa@users.noreply.github.com>
2023-07-19 04:24:25 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="22muppasa"
+original_name="cool"
+original_urlname="cool"
+original_description="Awesome cool created by 22muppasa"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv cool $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,9 @@
 import sys
 import pytest
 
-from project_name import create_app
-from project_name.ext.commands import populate_db
-from project_name.ext.database import db
+from cool import create_app
+from cool.ext.commands import populate_db
+from cool.ext.database import db
 
 
 @pytest.fixture(scope="session")
